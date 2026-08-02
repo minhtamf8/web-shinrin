6282fc40cfe340f824b7a738087d682b8f1c09d9
nguyentam1129 <tam.nm1129@gmail.com>
2026-08-02 12:02:18 +0700

    language

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -34,3 +34,35 @@ document.querySelectorAll(".featured-products__card").forEach((card) => {
 
     body.style.setProperty("--product-color", colors[product]);
 });
+
+/** language */
+
+const language = document.getElementById("language");
+
+const current = language.querySelector(".language__current");
+
+current.addEventListener("click", (e) => {
+    e.stopPropagation();
+
+    language.classList.toggle("language--open");
+});
+
+document.addEventListener("click", () => {
+    language.classList.remove("language--open");
+});
+
+const options = language.querySelectorAll(".language__option");
+
+options.forEach((option) => {
+    option.addEventListener("click", () => {
+        const img = option.querySelector("img");
+
+        const currentFlag = language.querySelector(".language__current img");
+
+        currentFlag.src = img.src;
+
+        currentFlag.alt = img.alt;
+
+        language.classList.remove("language--open");
+    });
+});
